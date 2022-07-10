0c0ca15f56f22c07745076dfeb88bbf104f083cf
ProjectByKumar <107754747+ProjectByKumar@users.noreply.github.com>
2022-07-11 02:14:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("this is my first jenkins integration with Git HUb");
+		System.out.println("------this is my first jenkins integration with Git HUb__-------");
 	}
 }
